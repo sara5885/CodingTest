bda945ea0fc43c42352b4f2010db11d080c0140a
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-05 15:10:32 +0900

    Title: 숫자로 이루어진 사각형, Time: 70ms, Memory: 19MB, Status: Passed - Codetree

--- 250905/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 250905/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -4,6 +4,8 @@ def print_one_rect(n):
     i=1
     for _ in range(n):
         for _ in range(n):
+            if i%10==0:
+                i+=1
             print(i%10,end=" ")
             i+=1
         print('')
