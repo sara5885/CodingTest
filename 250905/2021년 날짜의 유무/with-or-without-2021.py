fbb378ad2de977dbc20ecd1cd6b4cf9b5d0a9f69
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-05 17:19:20 +0900

    Title: 2021년 날짜의 유무, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250905/2021년 날짜의 유무/with-or-without-2021.py
+++ 250905/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,10 +1,15 @@
 M, D = map(int, input().split())
-
+arr_31=[1,3,5,7,8,10,12]
+arr_30=[4,6,9,11]
 def is_date(M,D):
-    if M>12 :
+    if M>12:
         return False 
-    if D>31:
-        return False 
+    if M in arr_31:
+        if D>31:
+            return False 
+    if M in arr_30:
+        if D>30:
+            return False 
     if M==2 :
         if D>28:
             return False 
