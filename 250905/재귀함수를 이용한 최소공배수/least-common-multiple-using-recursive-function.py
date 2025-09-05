915e7d15a60ecfa93f50f086ff9bda2f271f09f4
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-05 20:04:52 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250905/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def find_gcd(a,b):
+    while b!=0:
+        a,b=b,a%b 
+    return a
+def compute_abs(a,b):
+    global arr 
+    return arr[a]*arr[b]/find_gcd(arr[a],arr[b])
+
+tmp=0
+def find_abs(tmp_abs,i):
+    global arr 
+    global tmp 
+    #print(f"find_abs({i})")
+    if i==0:
+        return int(tmp)
+    tmp=arr[i]*tmp_abs/find_gcd(arr[i],tmp_abs)
+    return find_abs(tmp,i-1)
+
+
+
+# current_abs, idx 
+print(find_abs(1,n-1))    
