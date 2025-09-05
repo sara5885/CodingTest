68205de30da973e1ec24592e20b870343efac4ee
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-05 20:56:36 +0900

    Title: k번째로 신기한 문자열, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250905/k번째로 신기한 문자열/kth-special-string.py
+++ 250905/k번째로 신기한 문자열/kth-special-string.py
@@ -4,8 +4,13 @@ str = [input() for _ in range(n)]
 tmp_arr=[]
 # Please write your code here.
 for i in str:
-   if t in i:
-    tmp_arr.append(i)
+   #if t in i:
+    is_in=True 
+    for j in range(len(t)):
+        if t[j]!=i[j]:
+            is_in=False 
+    if is_in:
+        tmp_arr.append(i)
 
 tmp_arr.sort()
 
