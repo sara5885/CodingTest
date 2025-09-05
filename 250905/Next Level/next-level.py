705fab74ea8e4f52a83df5083dbfd8f96ccfc85d
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-05 23:35:55 +0900

    Title: Next Level, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250905/Next Level/next-level.py
@@ -0,0 +1,8 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+# Please write your code here.
+user1_id,user1_level=('codetree',10)
+
+print(f"user {user1_id} lv {user1_level}")
+print(f"user {user2_id} lv {user2_level}")
