4bc1d6c26f9d3ec001c54f5a660d03325189a981
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-06 14:28:00 +0900

    Title: 만나는 그 순간, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250906/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,51 @@
+n, m = map(int, input().split())
+
+d = []
+t = []
+for _ in range(n):
+    direction, time = input().split()
+    d.append(direction)
+    t.append(int(time))
+
+d2 = []
+t2 = []
+for _ in range(m):
+    direction, time = input().split()
+    d2.append(direction)
+    t2.append(int(time))
+
+# Please write your code here.
+location_per_sec1=[]
+location_per_sec2=[]
+location_per_sec1.append(500000)
+location_per_sec2.append(500000)
+now_loc=500000
+for i in range(n):
+    tmp_d=d[i] #direction
+    tmp_t=t[i] #time 
+    if d[i]=='L': 
+        for j in range(tmp_t):
+            location_per_sec1.append(now_loc-1)
+            now_loc-=1
+    elif d[i]=='R':
+        for j in range(tmp_t):
+            location_per_sec1.append(now_loc+1)
+            now_loc+=1
+now_loc=500000
+for i in range(m):
+    tmp_d=d2[i] #direction
+    tmp_t=t2[i] #time 
+    if d2[i]=='L': 
+        for j in range(tmp_t):
+            location_per_sec2.append(now_loc-1)
+            now_loc-=1
+    elif d2[i]=='R':
+        for j in range(tmp_t):
+            location_per_sec2.append(now_loc+1)
+            now_loc+=1
+#print(location_per_sec1)
+#print(location_per_sec2)
+for i in range(1, min(len(location_per_sec1),len(location_per_sec2))):
+    if location_per_sec1[i]==location_per_sec2[i]:
+        print(i)
+        break
