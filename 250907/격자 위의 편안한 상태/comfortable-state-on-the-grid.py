093a5c349d3ce3a9839e1a8eb864e23662558ae4
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-07 15:52:29 +0900

    Title: 격자 위의 편안한 상태, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250907/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+points = [tuple(map(int, input().split())) for _ in range(m)]
+
+grid=[
+    [0 for _ in range(m+1)]
+    for _ in range(n+1)
+]
+
+dx,dy=[1,-1,0,0],[0,0,1,-1]
+
+def in_range(r,c):
+    return r>=1 and r<=n and c>=1 and c<=m
+
+for i in range(m):
+    r,c=points[i]
+    grid[r][c]=1
+    cnt=0
+    for j in range(4):
+        if in_range(r+dx[j],c+dy[j]) and grid[r+dx[j]][c+dy[j]]==1:
+            cnt+=1
+    if cnt==3:
+        print(1)
+    else:
+        print(0)
+
+
