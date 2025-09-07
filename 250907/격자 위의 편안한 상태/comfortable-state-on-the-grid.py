07a8d447ae8346f79f70a31b2a929bb001c17129
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-07 16:02:39 +0900

    Title: 격자 위의 편안한 상태, Time: 109ms, Memory: 20MB, Status: Passed - Codetree

--- 250907/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 250907/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -2,14 +2,14 @@ n, m = map(int, input().split())
 points = [tuple(map(int, input().split())) for _ in range(m)]
 
 grid=[
-    [0 for _ in range(m+1)]
+    [0 for _ in range(n+1)]
     for _ in range(n+1)
 ]
 
 dx,dy=[1,-1,0,0],[0,0,1,-1]
 
 def in_range(r,c):
-    return r>=1 and r<=n and c>=1 and c<=m
+    return r>=1 and r<=n and c>=1 and c<=n
 
 for i in range(m):
     r,c=points[i]
@@ -18,6 +18,7 @@ for i in range(m):
     for j in range(4):
         if in_range(r+dx[j],c+dy[j]) and grid[r+dx[j]][c+dy[j]]==1:
             cnt+=1
+
     if cnt==3:
         print(1)
     else:
