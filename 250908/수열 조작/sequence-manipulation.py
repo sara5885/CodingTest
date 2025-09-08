b590043006aaecbd00247a233b84c5a503a424ff
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-08 11:46:26 +0900

    Title: 수열 조작, Time: 64ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250908/수열 조작/sequence-manipulation.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+# Please write your code here.
+from collections import deque 
+
+dq=deque()
+for i in range(1,n+1):
+    dq.append(i)
+
+while len(dq)>1:
+    dq.popleft()
+    dq.append(dq[0])
+    dq.popleft()
+
+print(dq[0])
