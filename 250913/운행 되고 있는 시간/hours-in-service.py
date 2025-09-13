5ad479d2a4d83fcdc133e4b70c9a06465cd9b8d6
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-13 11:19:13 +0900

    Title: 운행 되고 있는 시간, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250913/운행 되고 있는 시간/hours-in-service.py
+++ 250913/운행 되고 있는 시간/hours-in-service.py
@@ -4,14 +4,15 @@ a = [t[0] for t in times]
 b = [t[1] for t in times]
 
 total_max_time=0
-
 for i in range(n):
-    total_time=0
+    arr=[0]*1001
     for j in range(n):
-        if i==j: 
+        if i==j:
             continue 
-        total_time+=b[j]-b[i]
+        for k in range(a[j],b[j]):
+            arr[k]=1 
+    total_time=sum(arr)
     total_max_time=max(total_max_time,total_time)
 
 print(total_max_time)
-        
+
