ac4c00ef2075a2f403b823371eb6ea917bd1366e
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-13 23:32:30 +0900

    Title: 이상한 폭탄 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250913/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,16 @@
+#250913 (23:26)
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+bomb_arr=[]
+
+for i in range(N):
+    for j in range(i+1,N):
+        if num[i]==num[j] and abs(i-j)<=K:
+            bomb_arr.append(num[i])
+
+bomb_arr=bomb_arr[::-1]
+if len(bomb_arr)==0:
+    print(-1)
+else:
+    print(bomb_arr[0])
