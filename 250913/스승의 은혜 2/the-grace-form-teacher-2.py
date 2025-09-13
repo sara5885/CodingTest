15ac4519b9358b6360305a6b6ba547572d020719
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-13 16:07:09 +0900

    Title: 스승의 은혜 2, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250913/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 250913/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,32 +1,25 @@
 #250913 (11:27)
+#  반값할인받을 i의 반값가격으로 arr에 저장해야함  
 N, B = map(int, input().split())
 P = [int(input()) for _ in range(N)]
 
 max_cnt=0
 
-arr=sorted(P)
-
 # 모든 조합 
 for i in range(N):
     total=B
     tmp_cnt=0
-    # i의 선물을 반값 할인 받음 
-    # 이 때 선물 가능 최대 명수 
-    for j in range(N):
-        if i==j:
-            if total>=P[j]//2:
-                total-=P[j]//2
-                tmp_cnt+=1
-            else:
-                break
+    arr=[]
+    for j in P:
+        arr.append(j)
+    arr[i]=arr[i]//2
+    arr.sort()
+    for j in arr:
+        if total<j:
+            break
         else:
-            if total>=P[j]:
-                total-=P[j]
-                tmp_cnt+=1
-            else:
-                break 
-        
-        
+            total-=j
+            tmp_cnt+=1
     max_cnt=max(max_cnt,tmp_cnt)
     
 print(max_cnt)
