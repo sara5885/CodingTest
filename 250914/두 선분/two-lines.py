b1b2456739c0f8a50ff6c9b929fd5d75f53b9921
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-14 17:32:45 +0900

    Title: 두 선분, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250914/두 선분/two-lines.py
+++ 250914/두 선분/two-lines.py
@@ -1,9 +1,9 @@
 # 250914 (17:29)
 x1, x2, x3, x4 = map(int, input().split())
 
-if x1<x3 and x2>=x3 :
+if x1<=x3 and x2>=x3 :
     print('intersecting')
-elif x3<x1 and x4>=x1:
+elif x3<=x1 and x4>=x1:
     print('intersecting')
 else:
     print('nonintersecting')
