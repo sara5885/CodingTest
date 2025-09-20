565b3d06dfd598868210e1a207b781b2840a43eb
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-20 16:27:31 +0900

    Title: 마을 구분하기, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250920/마을 구분하기/seperate-village.py
+++ 250920/마을 구분하기/seperate-village.py
@@ -9,11 +9,13 @@ visited=[[0]*n for _ in range(n)]
 dx,dy=[-1,1,0,0],[0,0,-1,1]
 
 def dfs(r,c):
+    
     visited[r][c]=1
     cnt=1
     for idx in range(4):
         tmp_r,tmp_c=r+dx[idx],c+dy[idx]
-        if 0<=tmp_r<n and 0<=tmp_c<n and visited[tmp_r][tmp_c]==0:
+        if 0<=tmp_r<n and 0<=tmp_c<n and visited[tmp_r][tmp_c]==0 and grid[tmp_r][tmp_c]==1:
+            visited[tmp_r][tmp_c]=1
             cnt+=dfs(tmp_r,tmp_c)
     return cnt 
 
@@ -21,10 +23,12 @@ def dfs(r,c):
 for i in range(n):
     for j in range(n):
         town_size=0
-        if visited[i][j]==0:
+        if visited[i][j]==0 and grid[i][j]==1:
             town_size=dfs(i,j)
-        if town_size!=0:
-            all_town.append(town_size)
+            if town_size!=0:
+                all_town.append(town_size)
 
-print(al)
-
+all_town.sort()
+print(len(all_town))
+for i in all_town:
+    print(i)
