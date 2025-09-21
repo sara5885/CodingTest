01fee9d7076f0babffd4665019f4e75455244764
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-09-21 14:16:41 +0900

    Title: 각 정점까지의 최단 경로, Time: 568ms, Memory: 39MB, Status: Passed - Codetree

--- 250921/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 250921/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -3,7 +3,7 @@ import sys
 import heapq 
 n, m = map(int, input().split())
 k = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+# edges = [tuple(map(int, input().split())) for _ in range(m)]
 
 source=k 
 dist=[sys.maxsize]*(n+1)
@@ -11,18 +11,22 @@ dist[k]=0
 
 pq=[(0,k)] #start point 
 
+# 많은 edge를 대비해서 adjacent list 사용하기
+graph=[[]for _ in range(n+1)]
+for _ in range(m):
+    u,v,w=map(int,input().split())
+    graph[u].append((v,w))
+    graph[v].append((u,w))
+
+
+
 while pq:
     tmp_dist,tmp_node=heapq.heappop(pq)
     if dist[tmp_node]!=tmp_dist : continue 
-    for i in range(m):
-        s_n,d_n,value=edges[i]
-        if tmp_node!=s_n and tmp_node!=d_n:
-            continue 
-        elif tmp_node==d_n:
-            s_n,d_n=d_n,s_n
-        if dist[d_n]>dist[s_n]+value:
-            dist[d_n]=dist[s_n]+value 
-            heapq.heappush(pq,(dist[d_n],d_n))
+    for v,w in graph[tmp_node]: # 해당 node의 인접 노드만 확인하기 
+        if dist[v]>dist[tmp_node]+w:
+            dist[v]=dist[tmp_node]+w 
+            heapq.heappush(pq,(dist[v],v))
 
 for i in range(1,n+1):
     if dist[i]==sys.maxsize:
