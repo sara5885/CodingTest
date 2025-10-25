e4dc4dcb24cb48ef33981e7256e30a80329cb635
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-25 14:28:30 +0900

    Title: 트리의 부모 노드, Time: 335ms, Memory: 100MB, Status: Memory Limit Exceed - Codetree

---
+++ 251025/트리의 부모 노드/parent-node-of-the-tree.py
@@ -0,0 +1,21 @@
+# 251025 (14:21)
+n = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+parent_arr=[0]*(n+1)
+grid=[[0]*(n+1) for _ in range(n+1)]
+visited=[0]*(n+1)
+for i,j in edges:
+    grid[i][j]=1 
+    grid[j][i]=1 
+
+def dfs(node):
+    visited[node]=1
+    for i in range(n+1):
+        if not visited[i] and grid[node][i]:
+            parent_arr[i]=node 
+            visited[i]=1 
+            dfs(i)
+
+dfs(1)
+for i in range(2,n+1):
+    print(parent_arr[i])
