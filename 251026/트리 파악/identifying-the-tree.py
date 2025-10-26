4a483f60dadeabc0db78584525b443b4510561d6
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-26 15:30:21 +0900

    Title: 트리 파악, Time: 386ms, Memory: 43MB, Status: Runtime Error - Codetree

--- 251026/트리 파악/identifying-the-tree.py
+++ 251026/트리 파악/identifying-the-tree.py
@@ -1,27 +1,32 @@
 #251016 (14:00)
-# 꼭 visited가 있어야 하는가. 
+# total_depth_sum 누적 합 : dfs 안에서 leaf node인 경우 더하기 
 n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 graph=[[] for _ in range(n+1)]
 visited=[0]*(n+1)
+ans=0
 for a,b in edges:
     graph[a].append(b)
+    graph[b].append(a)
 
-def dfs(node):
+def dfs(node,depth):
+    global ans
     visited[node]=1
     cnt=1 
+    leaf_flag=True 
     for i in graph[node]:
         if not visited[i]:
             visited[i]=1 
-            cnt+=dfs(i)
-    return cnt 
-
+            leaf_flag=False 
+            dfs(i,depth+1)
+    if leaf_flag:#leaf인경우에만 depth더하기 
+        ans+=depth 
 
 
 # 루트-leaf노드까지 총 거리 
-cnt=dfs(1)
+dfs(1,0)
 # print(cnt)
-if (cnt-1)%2==0:
+if (ans)%2==0:
     print(0)
 else:
     print(1)
