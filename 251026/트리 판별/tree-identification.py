3c12c68200868b33ab11d304cf3f6d2986a2f26a
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-26 12:53:48 +0900

    Title: 트리 판별, Time: 64ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251026/트리 판별/tree-identification.py
+++ 251026/트리 판별/tree-identification.py
@@ -6,13 +6,24 @@ graph=[[] for _ in range(10001)]
 #누군가의 자식으로 이미 선정된 node 
 child=set()
 tree_flag=True 
-
+node=[]
+root_cand=[]
 for a,b in edges:
+    if a not in child and a not in root_cand:
+        root_cand.append(a)
     if b in child:
         tree_flag=False 
         break
+    if b in root_cand:
+        root_cand.remove(b)
     graph[a].append(b)
     child.add(b)
+    node.append(a)
+    node.append(b)
+
+# print(root_cand)
+if len(root_cand)!=1:
+    tree_flag=False 
 
 if tree_flag:
     print(1)
