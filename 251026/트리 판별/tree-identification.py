14cfcf9789aa18a91f28bbf9ca3e6eeb49e0fa2a
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-26 13:10:30 +0900

    Title: 트리 판별, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251026/트리 판별/tree-identification.py
+++ 251026/트리 판별/tree-identification.py
@@ -6,7 +6,7 @@ graph=[[] for _ in range(10001)]
 #누군가의 자식으로 이미 선정된 node 
 child=set()
 tree_flag=True 
-node=[]
+node=set()
 root_cand=[]
 for a,b in edges:
     if a not in child and a not in root_cand:
@@ -18,12 +18,28 @@ for a,b in edges:
         root_cand.remove(b)
     graph[a].append(b)
     child.add(b)
-    node.append(a)
-    node.append(b)
+    node.add(a)
+    node.add(b)
+# 2개이상의 그래프. 어떤 그래프가 cycle일 때. 
+# dfs로 모든 그래프 도달 가능한지 확인 
+visited=[0]*10001
+def dfs(node):
+    cnt=1 
+    visited[node]=1
+    for i in graph[node]:
+        if not visited[i]:
+            visited[i]=1
+            cnt+=dfs(i)
+    return cnt 
+
 
 # print(root_cand)
 if len(root_cand)!=1:
-    tree_flag=False 
+    tree_flag=False
+else:
+    ans=dfs(root_cand[0])
+    if ans!=len(node):
+        tree_flag=False
 
 if tree_flag:
     print(1)
