47fdac9792e6f01a75f4e0ee75ddee1631291336
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-27 23:04:32 +0900

    Title: 최대 증가 부분 수열, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251027/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,22 @@
+# 251027 (23:00)
+import sys 
+INT_MIN=-sys.maxsize 
+n = int(input())
+m = list(map(int, input().split()))
+
+# dp
+# dp[i]= 이전 값들 
+# 조건 : arr[i] > arr[j] 
+# dp
+dp=[INT_MIN]*(n)
+dp[0]=1 
+for i in range(1,n):
+    for j in range(0,i):
+        if m[i]>m[j]:
+            dp[i]=max(dp[i],dp[j]+1)
+
+ans=0
+for i in range(n):
+    ans=max(dp[i],ans)
+# print(dp)
+print(ans)
