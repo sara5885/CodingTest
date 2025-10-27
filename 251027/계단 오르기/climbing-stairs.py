16e9fa7cc9ebc4382f50549fefc08b118cda8aa7
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-27 21:33:23 +0900

    Title: 계단 오르기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251027/계단 오르기/climbing-stairs.py
+++ 251027/계단 오르기/climbing-stairs.py
@@ -1,13 +1,21 @@
 #251027 (21:28)
+import sys
+sys.setrecursionlimit(1000000)
 n = int(input())
-
+memo=[-1]*(n+1)
 def stair(n):
+    if memo[n]!=-1:
+        return memo[n]
     if n==1:
+        memo[1]=0
         return 0
     if n==2:
+        memo[2]=1
         return 1
     if n==3:
+        memo[3]=1
         return 1 
-    return stair(n-2)+stair(n-3)
+    memo[n]=stair(n-2)+stair(n-3)
+    return memo[n]
 
 print(stair(n)%10007)
