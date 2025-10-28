cd55d0da21156647906bcc390939608b00fc3521
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-28 20:36:37 +0900

    Title: 최대 동전 거슬러주기, Time: 62ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251028/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,19 @@
+# 251028 (20:23)
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+
+# max coin 
+# dp[i]: 금액 i를 만드는 동전의 최대 개수 
+# for j in range(M)
+    # if i>=coin[j]:
+        #dp[i]=max(dp[i-coin[j]]+1, dp[i])
+dp=[0]*(M+1)
+for i in range(M+1):
+    for j in range(N):
+        if i>=coin[j]:
+            dp[i]=max(dp[i], dp[i-coin[j]]+1)
+
+if dp[M]==0:
+    print(-1)
+else:
+    print(dp[M])
