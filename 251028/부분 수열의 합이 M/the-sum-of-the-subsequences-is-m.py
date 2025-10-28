ec6d6e83eefd04f81844618e414cf4d69f9720ef
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-28 20:07:24 +0900

    Title: 부분 수열의 합이 M, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251028/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 251028/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -1,18 +1,14 @@
-#251028 (19:26)
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 dp=[101]*(m+1)
-dp[0]=0
-
-for i in range(n): # 수열 길이 (수열 idx)
-    for j in range(m,-1,-1): # 총 합 
-        if j>=A[i]:
-            if dp[j-A[i]]==101:
-                continue 
+dp[0]=0 
+for i in range(n):
+    for j in range(m,-1,-1):
+        if dp[j-A[i]]!=101:
             dp[j]=min(dp[j], dp[j-A[i]]+1)
-ans=dp[m]
-if ans==101:
-    ans=-1 
-print(ans)
 
+if dp[m]==101:
+    print(-1)
+else:
+    print(dp[m])
