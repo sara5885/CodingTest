3c56e373673b1c3a7a8a5f8af1061c7e399906f7
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-28 22:33:44 +0900

    Title: 최소 와이파이 수, Time: 1088ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

---
+++ 251028/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,15 @@
+#251028 (22:22)
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# 1 1 0 1 1 1 
+# 거리 1 
+# 사람 살고있으면 거기서 +m에 wifi 놓기 / wifi 놓은 지점 +m에서 다시시작 
+cnt,i=0,0 
+while i<n:
+    if arr[i]:
+        cnt+=1
+        i+=2*m+i 
+    else:
+        i+=1 
+print(cnt)
