9f1b0b832fd1570d3bcdc6859ed630ee2e6ddc74
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-29 18:04:23 +0900

    Title: 수 합치기, Time: 213ms, Memory: 23MB, Status: Passed - Codetree

--- 251029/수 합치기/merge-numbers.py
+++ 251029/수 합치기/merge-numbers.py
@@ -1,16 +1,18 @@
 #251029 (17:49)
+import heapq 
 n = int(input())
 arr = list(map(int, input().split()))
-
+heapq.heapify(arr)
 total_cost=0
 while len(arr)>1:
-    arr.sort()
-    min1=arr[0]
-    min2=arr[1]
+    # arr.sort()
+    # min1=arr[0]
+    # min2=arr[1]
+    min1=heapq.heappop(arr)
+    min2=heapq.heappop(arr)
     # print(min1,min2)
     total_cost+=(min1+min2)
-    arr.append(min1+min2)
-    del arr[0]
-    del arr[0]
+    
+    heapq.heappush(arr,min1+min2)
 
 print(total_cost)
