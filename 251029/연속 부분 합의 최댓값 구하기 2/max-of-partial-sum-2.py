6c1688880aa52213a5a05647719164411eb78072
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-29 17:18:21 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 251029/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 251029/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -6,7 +6,7 @@ a = list(map(int, input().split()))
 ans=-INT_MAX
 current_sum=0
 for i in a:
-    if current_sum+i <0:
+    if current_sum <0: #이전의 결과값을 기반으로 확인하기. 
         current_sum=i  #그냥 a부터 다시 초기화 
     else:
         current_sum+=i 
