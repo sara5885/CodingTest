cf1892fa56b0ab8258512d8b5950c2c52c1a02d0
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-29 14:29:35 +0900

    Title: 정수 분배하기, Time: 80ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251029/정수 분배하기/distributing-integers.py
@@ -0,0 +1,29 @@
+# 251029 (14:22)
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+# 같은 크기의 정수 k 를 m개 만들기 
+arr.sort()
+right=arr[0]
+left=1 
+
+ans=0
+
+def check_possible(x):
+    cnt=0
+    for a in arr:
+        cnt+=a//x 
+    if cnt>=m:
+        return True 
+    else:
+        return False 
+
+while left<=right:
+    mid=(right+left)//2 
+    if check_possible(mid):
+        left=mid+1 
+        ans=max(ans,mid)
+    else:
+        right=mid-1 
+
+print(ans)
