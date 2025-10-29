8707a19f7e93589657bb345351f0db6ab39baf81
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-29 17:41:40 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 285ms, Memory: 36MB, Status: Runtime Error - Codetree

---
+++ 251029/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,27 @@
+# 251029 (17:19)
+n, m = map(int, input().split())
+w, v = zip(*[tuple(map(int, input().split())) for _ in range(n)])
+w, v = list(w), list(v)
+
+arr=[]
+for i in range(n):
+    arr.append([v[i]/w[i],i])
+
+arr.sort(key=lambda x: -x[0])
+# arr 순서대로 담아주기 : 
+j_idx=0
+value=0
+while m>0:
+    j_num=arr[j_idx][1]
+    j_price=v[j_num]
+    j_weight=w[j_num]
+    if m>j_weight: #모두 담을 수 있는 것 
+        m-=j_weight
+        value+=j_price
+    else: # 쪼개서 담기 
+        value+=m*arr[j_idx][0]
+        m=0
+    j_idx+=1 
+
+print(f"{value:.3f}")
+
