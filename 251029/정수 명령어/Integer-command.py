094a5a2e608409529eb7240cdbbf865b49a05cad
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-29 18:26:25 +0900

    Title: 정수 명령어, Time: 92ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 251029/정수 명령어/Integer-command.py
@@ -0,0 +1,28 @@
+#251029 (18:22)
+from sortedcontainers import SortedSet 
+T = int(input())
+s=SortedSet()
+for _ in range(T):
+    k = int(input())
+    operations = [tuple(input().split()) for _ in range(k)]
+    command = [op[0] for op in operations]
+    n = [int(op[1]) for op in operations]
+
+    # Please write your code here.
+    for i in range(k):
+        if command[i]=='I':
+            s.add(n[i])
+        elif command[i]=='D':
+            if len(s)==0:
+                continue 
+            if n[i]==1:
+                s.remove(s[-1])
+            elif n[i]==-1:
+                s.remove(s[0])
+
+    if len(s)==0:
+        print('EMPTY')
+    else:
+        print(s[-1],s[0])
+
+            
