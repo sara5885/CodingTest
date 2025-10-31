f8db5e8989a9c558a31b51ac3690970fe14f701b
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-31 17:22:54 +0900

    Title: 가장 짧은 부분합, Time: 48ms, Memory: 18MB, Status: Passed - Codetree

--- 251031/가장 짧은 부분합/shortest-subtotal.py
+++ 251031/가장 짧은 부분합/shortest-subtotal.py
@@ -6,18 +6,17 @@ arr = [0]+list(map(int, input().split()))
 
 ans=INT_MAX 
 
+
 sum_val=0
-j=0
+j=1
 for i in range(1,n+1):
-    while j+1<=n and sum_val<s:
-        sum_val+=arr[j+1]
+    while j<=n and sum_val<s:
+        sum_val+=arr[j]
         j+=1 
     if sum_val<s:
         break 
-    ans=min(ans,j-i+1)
-    sum_val-=arr[i] #다시 처음부터 매번 더하는게 아니라 sum_val에서 뒤에 추가하고 맨앞삭제하고 
-
-
+    ans=min(j-i,ans)
+    sum_val-=arr[i]
 if ans==INT_MAX:
     ans=-1 
 print(ans)
