376d111c89d769d97f051a288fe4718b52538fa9
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-31 15:41:24 +0900

    Title: 마라톤 중간에 택시타기, Time: 220ms, Memory: 30MB, Status: Passed - Codetree

--- 251031/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
+++ 251031/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -1,3 +1,5 @@
+import sys 
+INT_MAX=sys.maxsize 
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
@@ -12,7 +14,7 @@ for i in range(n-2,-1,-1):
     r[i]=r[i+1]+abs(x[i+1]-x[i])+abs(y[i+1]-y[i])
 # print(l)
 # print(r)
-min_dist=10000000
+min_dist=INT_MAX
 for i in range(1,n-1):
     tmp_dist=l[i-1]+r[i+1]+abs(x[i+1]-x[i-1])+abs(y[i+1]-y[i-1])
     if tmp_dist<min_dist:
