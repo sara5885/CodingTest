e6c32967994db0498f21c35d8aaca6de91c2b70e
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-31 14:39:56 +0900

    Title: 연속한 K개의 숫자, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251031/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 251031/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -7,12 +7,14 @@ for i in range(1,N+1):
     if i not in missing:
         exist[i]=1 
 max_cnt=0
+prefix_sum=[0]*(N+1)
+for i in range(1,N+1):
+    prefix_sum[i]=prefix_sum[i-1]+exist[i]
 for i in range(1,N+2-K):
-    if sum(exist[i:i+K])>max_cnt:
-        max_cnt=sum(exist[i:i+K])
-        # print(i)
-        # print(exist[i:i+K])
-        # print(max_cnt)
+    j=i+K-1
+    current_sum=prefix_sum[j]-prefix_sum[i-1]
+    if current_sum>max_cnt:
+        max_cnt=current_sum
 print(K-max_cnt)
 
 
