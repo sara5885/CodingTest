2bcc653657d335b08f94171927a1507543b7ff8b
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-10-31 12:52:28 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 1097ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

--- 251031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 251031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,13 +1,10 @@
 # 251031 (12:45)
 n = int(input())
 price = list(map(int, input().split()))
-arr=[]
+
+max_diff=0
 for i in range(n-1):
     for j in range(i+1,n):
         if price[j]-price[i]>0:
-            arr.append(price[j]-price[i])
-if len(arr)==0:
-    print(0)
-else:
-    arr.sort()
-    print(arr[-1])
+            max_diff=max(max_diff,price[j]-price[i])
+print(max_diff)
