470d2f9d44356bd77d8703f773eaa823adbb57eb
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-11-01 15:08:27 +0900

    Title: 최단 거리 3, Time: 248ms, Memory: 31MB, Status: Passed - Codetree

--- 251101/최단 거리 3/shortest-distance-3.py
+++ 251101/최단 거리 3/shortest-distance-3.py
@@ -7,8 +7,8 @@ A, B = map(int, input().split())
 
 graph=[[INT_MAX for _ in range(n+1)] for _ in range(n+1) ]
 for x,y,w in edges:
-    graph[x][y]=w 
-    graph[y][x]=w
+    graph[x][y]=min(graph[x][y],w)
+    graph[y][x]=min(graph[y][x],w)
 for i in range(1,n+1):
     graph[i][i]=0
 
@@ -19,7 +19,7 @@ heapq.heappush(pq,(0,A))
 
 while pq:
     tmp_dist, tmp_node= heapq.heappop(pq)
-    if dist_a[tmp_node]<tmp_dist:
+    if dist_a[tmp_node]!=tmp_dist:
         continue 
     for v in range(1,n+1):
         v_dist=tmp_dist+graph[tmp_node][v]
