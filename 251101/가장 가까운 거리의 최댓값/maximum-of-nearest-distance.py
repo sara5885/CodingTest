2a37fe053a68806d63b3390b638129ce3bb95a4c
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-11-01 15:39:43 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 3100ms, Memory: 54MB, Status: Time Limit Exceed - Codetree

---
+++ 251101/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.py
@@ -0,0 +1,40 @@
+#251101 (15:27)
+import heapq 
+import sys 
+INT_MAX=sys.maxsize 
+n, m = map(int, input().split())
+a, b, c = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+graph=[[] for _ in range(n+1)]
+
+for x,y,w in edges:
+    graph[x].append((y,w))
+    graph[y].append((x,w))
+ans=-1
+for i in range(1,n+1):
+    if i==a or i==b or i==c:
+        continue 
+    dist_i=[INT_MAX]*(n+1)
+    dist_i[i]=0
+    pq=[]
+    heapq.heappush(pq,(0,i))
+    while pq:
+        dist, node = heapq.heappop(pq)
+        if dist_i[node]!=dist : continue 
+
+        for v,w in graph[node]:
+            tmp_dist=dist_i[node]+w 
+            if tmp_dist<dist_i[v]:
+                dist_i[v]=tmp_dist 
+                heapq.heappush(pq,(tmp_dist,v))
+    min_in_i=INT_MAX 
+    for j in range(1,n+1):
+        if j==i: continue 
+        if j not in (a,b,c): continue
+        min_in_i=min(min_in_i,dist_i[j])
+    # print(i, dist_i)
+    # print(min_in_i)
+    if min_in_i>ans:
+        ans=min_in_i
+
+print(ans)
