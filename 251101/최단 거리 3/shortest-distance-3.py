820c98e13062836644ddd3a4c4c9bb861205bc9f
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-11-01 15:13:09 +0900

    Title: 최단 거리 3, Time: 174ms, Memory: 25MB, Status: Passed - Codetree

--- 251101/최단 거리 3/shortest-distance-3.py
+++ 251101/최단 거리 3/shortest-distance-3.py
@@ -5,25 +5,23 @@ n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 A, B = map(int, input().split())
 
-graph=[[INT_MAX for _ in range(n+1)] for _ in range(n+1) ]
+# Please write your code here.
+graph=[[] for _ in range(n+1)]
 for x,y,w in edges:
-    graph[x][y]=min(graph[x][y],w)
-    graph[y][x]=min(graph[y][x],w)
-for i in range(1,n+1):
-    graph[i][i]=0
+    graph[x].append((y,w))
+    graph[y].append((x,w))
 
 dist_a=[INT_MAX]*(n+1)
 dist_a[A]=0
-pq=[]
-heapq.heappush(pq,(0,A))
-
+pq=[(0,A)]
 while pq:
-    tmp_dist, tmp_node= heapq.heappop(pq)
-    if dist_a[tmp_node]!=tmp_dist:
-        continue 
-    for v in range(1,n+1):
-        v_dist=tmp_dist+graph[tmp_node][v]
-        if v_dist<dist_a[v]:
-            dist_a[v]=v_dist 
-            heapq.heappush(pq,(v_dist,v))
+    dist,node = heapq.heappop(pq)
+    if dist_a[node]!=dist:
+        continue #dummy node 
+    for v,w in graph[node]:
+        tmp_dist=w+dist 
+        if tmp_dist<dist_a[v]:
+            dist_a[v]=tmp_dist 
+            heapq.heappush(pq,(tmp_dist,v))
+
 print(dist_a[B])
