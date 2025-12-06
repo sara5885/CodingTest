b7f9218f1b96d338e8c2a9090feb0f9c5c13c9d6
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2025-12-06 14:04:01 +0900

    Title: 계단 수, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 251206/계단 수/stair-number.py
+++ 251206/계단 수/stair-number.py
@@ -2,9 +2,31 @@
 n = int(input())
 
 # Please write your code here.
-dp=[[0 for _ in range(10)] for _ in range(n+1)]
-for i in range(1,n+1):
-    dp[0][i]=1 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        dp[i][j]=dp[i-1][j-1] + dp[i-1][j+1]
+dp=[[0 for _ in range(10)] for _ in range(n)]
+for i in range(10):
+    if i==0:
+        dp[0][i]=0
+    else:
+        dp[0][i]=1 
+# for i in range(10):
+#     print(dp[0][i],end=" ")
+# print()
+
+for i in range(1,n):
+    for j in range(10):
+        if j==0:
+            dp[i][j]=dp[i-1][j+1]
+        elif j==9:
+            dp[i][j]=dp[i-1][j-1]
+        else:
+            dp[i][j]=dp[i-1][j-1] + dp[i-1][j+1]
+
+
+# for i in range(n):
+#     for j in range(10):
+#         print(dp[i][j],end=" ")
+#     print()
+ans=0
+for j in range(10):
+    ans+=dp[n-1][j]
+print(ans%(10**9 + 7))
