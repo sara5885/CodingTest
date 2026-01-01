5bfd2f7e8594c017129510c98565219caab09436
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-01 13:32:07 +0900

    Title: 기울어진 직사각형, Time: 117ms, Memory: 20MB, Status: Passed - Codetree

--- 260101/기울어진 직사각형/slanted-rectangle.py
+++ 260101/기울어진 직사각형/slanted-rectangle.py
@@ -2,28 +2,31 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-ans=0
+dir=0
 dx=[-1,-1,1,1]
 dy=[1,-1,-1,1]
+ans=0
+
 def get_score(i,j,h,w):
-    # start idx : i,j
+    len=[h,w,h,w]
     score=0
-    move_cnt=[h,w,h,w]
     cx,cy=i,j
-    # 순서대로 이동 
-    for dir in range(4): #
-        len=move_cnt[dir]
-        for _ in range(len): # h or w 길이만큼 
+    for dir in range(4): # direction을 막혔을 때 바뀌는게 아니고 해당 길이만큼 갔을 때 바꾸는 것 
+        c_len=len[dir]
+        for _ in range(c_len):
             nx,ny=cx+dx[dir], cy+dy[dir]
             if not (0<=nx<n and 0<=ny<n):
                 return -1 
             score+=grid[nx][ny]
-            cx,cy=nx,ny
+            cx,cy=nx,ny 
     return score 
 
 for i in range(n):
     for j in range(n):
         for h in range(1,n):
             for w in range(1,n):
-                ans=max(ans,get_score(i,j,h,w))
-print(ans)
+                score=get_score(i,j,h,w)
+                ans=max(ans,score)
+
+
+print(ans)  
