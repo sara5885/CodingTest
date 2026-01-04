a7836f9dec24c2a4785f8586d44c7a6cab3d33ad
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-04 15:58:59 +0900

    Title: 천 개의 정거장, Time: 994ms, Memory: 64MB, Status: Passed - Codetree

--- 260104/천 개의 정거장/thousand-stops.py
+++ 260104/천 개의 정거장/thousand-stops.py
@@ -6,7 +6,7 @@ A, B, N = map(int, input().split())
 bus_fare = []
 stop_count = []
 bus_stops = []
-graph=[[] for _ in range(1001)]
+graph=[[(INT_MAX,INT_MAX) for _ in range(1001)] for _ in range(1001)]
 for _ in range(N):
     fare, count = map(int, input().split())
     bus_fare.append(fare)
@@ -16,7 +16,8 @@ for _ in range(N):
         for j in range(i+1, count):
             a=bus_stops[-1][i]
             b=bus_stops[-1][j]
-            graph[a].append((b,fare,j-i))
+            if graph[a][b]>(fare,j-i):
+                graph[a][b]=(fare,j-i)
 
 # cost, time 
 dist=[(INT_MAX,INT_MAX)]*(1001)
@@ -26,12 +27,15 @@ while pq:
     c_node, (c_cost,c_time) = heapq.heappop(pq)
     if dist[c_node]< (c_cost,c_time):
         continue 
-    for n_node, n_cost, n_time in graph[c_node]:
-        new_cost=c_cost+n_cost
-        new_time=c_time+n_time
-        if dist[n_node]>(new_cost,new_time):
-            dist[n_node]=(new_cost,new_time)
-            heapq.heappush(pq,(n_node,(new_cost,new_time)))
+    for i in range(1,1001):
+        if graph[c_node][i]==(INT_MAX,INT_MAX):
+            continue 
+        (next_cost,next_time)=graph[c_node][i]
+        new_cost=c_cost+next_cost
+        new_time=c_time+next_time
+        if dist[i]>(new_cost,new_time):
+            dist[i]=(new_cost,new_time)
+            heapq.heappush(pq,(i,(new_cost,new_time)))
 
 a,b=dist[B]
 if (a,b)==(INT_MAX,INT_MAX):
