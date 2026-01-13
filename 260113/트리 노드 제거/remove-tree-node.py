1a16d873c9ed1934c8715b835951df6e7018f850
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-13 12:35:34 +0900

    Title: 트리 노드 제거, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260113/트리 노드 제거/remove-tree-node.py
+++ 260113/트리 노드 제거/remove-tree-node.py
@@ -6,21 +6,28 @@ parent = list(map(int, input().split()))
 k = int(input())
 visited=[0]*(n)
 removed_node=[0]*(n)
+root_num=-1
 
 graph=[[] for _ in range(n)]
 for i in range(n):
     p_num=parent[i]
-    graph[p_num].append(i)
+    if p_num==-1:
+        root_num=i
+    else: 
+        graph[p_num].append(i)
+
 # for i in graph:
 #     print(i)
 removed_node[k]=1 
 
 def count_leaf(node):
+    # print(node,"번째 node", end=" ")
     if node is None:
         return 0 
     if removed_node[node]:
         return 0
     if not graph[node]: #empty 
+        # print("leaf node")
         return 1 
     else:
         cnt=0
@@ -29,5 +36,5 @@ def count_leaf(node):
         return cnt 
 
 
-ans=count_leaf(0)
+ans=count_leaf(root_num)
 print(ans)
