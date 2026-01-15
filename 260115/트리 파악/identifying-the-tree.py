7e4d9dc5ecf6d81807ce7f704320957856464418
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-15 15:17:17 +0900

    Title: 트리 파악, Time: 391ms, Memory: 43MB, Status: Runtime Error - Codetree

--- 260115/트리 파악/identifying-the-tree.py
+++ 260115/트리 파악/identifying-the-tree.py
@@ -1,51 +1,32 @@
-# 260115 (12:03)
-from collections import deque 
 n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
+# Please write your code here.
+visited=[0]*(n+1)
+depth=[0]*(n+1)
 graph=[[] for _ in range(n+1)]
-lived=deque()
-parent=[0]*(n+1)
+ans=0
 
 for i,j in edges:
     graph[i].append(j)
     graph[j].append(i)
 
-for i in range(2,n+1):
-    if len(graph[i])==1:
-        # print(i, graph[i])
-        lived.append(i)
+def dfs(x):
+    global ans 
+    is_leaf=True 
+    for y in graph[x]:
+        if visited[y]: continue 
+        visited[y]=1
+        is_leaf=False
+        depth[y]=depth[x]+1
+        dfs(y) 
+    if is_leaf: ans+=depth[x]
 
-def set_parent(node):
-    global parent
-    parent[node]=-1 #자기자신 
-    pq=deque([node])
+visited[1]=1
+dfs(1)
 
-    while pq:
-        n=pq.popleft()
-        for child in graph[n]:
-            if not parent[child]:
-                parent[child]=n
-                pq.append(child)
-set_parent(1)
-
-
-turn = 'A'
-while lived:
-    node=lived.pop()
-    if parent[node]!=1:
-        lived.append(parent[node])
-
-    if turn=='A':
-        turn='B'
-    else: 
-        turn='A'
-
-if turn=='B':
+if ans%2==1:
     print(1)
 else:
     print(0)
-
-
-
 
