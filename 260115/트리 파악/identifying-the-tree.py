445ecbe9059ce3b382ee359d5e5e6f7339c3f8d9
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-15 15:23:32 +0900

    Title: 트리 파악, Time: 711ms, Memory: 137MB, Status: Passed - Codetree

--- 260115/트리 파악/identifying-the-tree.py
+++ 260115/트리 파악/identifying-the-tree.py
@@ -2,33 +2,30 @@ import sys
 sys.setrecursionlimit(100000)
 n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
-
-# Please write your code here.
-visited=[0]*(n+1)
-depth=[0]*(n+1)
 graph=[[] for _ in range(n+1)]
-ans=0
-
+visited=[0]*(n+1)
+# root - 모든 leaf node까지의 거리의 합 
+dist=[0]*(n+1) #루트에서 i까지의 dist 
+ans=0 
 for i,j in edges:
     graph[i].append(j)
     graph[j].append(i)
 
-def dfs(x):
+def dfs(node):
     global ans 
+    visited[node]=1
     is_leaf=True 
-    for y in graph[x]:
-        if visited[y]: continue 
-        visited[y]=1
-        is_leaf=False
-        depth[y]=depth[x]+1
-        dfs(y) 
-    if is_leaf: ans+=depth[x]
+    for i in graph[node]:
+        if visited[i] : continue 
+        is_leaf=False 
+        visited[i]=1 
+        dist[i]=dist[node]+1 
+        dfs(i)
+    if is_leaf:
+        ans+=dist[node]
 
-visited[1]=1
 dfs(1)
-
 if ans%2==1:
     print(1)
 else:
     print(0)
-
