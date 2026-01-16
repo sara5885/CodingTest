c8a71c90f747c7e45728c44ef19347137a0fe331
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-16 12:31:29 +0900

    Title: 트리 사촌, Time: 180ms, Memory: 54MB, Status: Wrong Answer - Codetree

--- 260116/트리 사촌/beard-tree.py
+++ 260116/트리 사촌/beard-tree.py
@@ -1,53 +1,52 @@
-# 251026 (15:32)
-from collections import deque
+#260116 (12:00)
+from collections import deque 
 n, k = map(int, input().split())
 sequence = list(map(int, input().split()))
-root=-1 
-k_idx=-1 
-graph=[[] for _ in range(1000001)] 
-leaf=deque()
-# child=[[] for _ in range(n+1)]
-c_node=-1
-parent=-1 
-depth=[]
+graph=[[] for _ in range(1000001)]
+depth={}
+parent={}
+leaf=deque([sequence[0]])
 root=sequence[0]
-idx=0
-depth.append([])
-depth[idx].append(root)
-leaf.append(root)
+
+node=root 
 for i in range(1,n):
-    if sequence[i]-1!=sequence[i-1]:
-        idx+=1 
-        depth.append([])
-        c_node=leaf.popleft()
-        graph[c_node].append(sequence[i])
+    # +1 차이 
+    if sequence[i] -1 == sequence[i-1]:
+        graph[node].append(sequence[i])
+    # 더 많은 차이 
     else:
-        graph[c_node].append(sequence[i])
-        # parent[sequence[i]]=c_node
-    depth[idx].append(sequence[i])
-    if sequence[i]==k : 
-        parent=c_node
-        k_idx=idx 
+        node=leaf.popleft()
+        graph[node].append(sequence[i])
+    parent[sequence[i]]=node
     leaf.append(sequence[i])
-        # 1. 연속된 수 (+1)라서 얘네는 자식 됨? 
-        # 2. 연속된 수가 아니다 sequence[i]+1!=sequence[i+1]
-            # 
 
-# 정답 
-# p=parent[node]
-# print(k, k_idx, depth[k_idx])
-# print(len(depth[k_idx])-1) # : node의 부모의 graph list의 갯수 (본인 제외)
-# for i in depth:
-#     print(i)
 
-# 그냥 dfs 할까
+# depth 설정하기 
+depth[root]=0 
+def bfs():
+    pq=deque()
+    pq.append(root)
+    visited=set()
+    visited.add(root)
+
+    while pq:
+        n=pq.popleft()
+        for i in graph[n]:
+            if i not in visited :
+                depth[i]=depth[n]+1 
+                visited.add(i)
+                pq.append(i)
+        
+bfs()
+# for node,i in depth.items():
+#     print(node, i)
+
+# print(depth)
+# print(parent)
 ans=0
-visited=set()
-def dfs(node,target,len):
-    global ans 
-    for i in graph[node]:
-        if i in visited:
-            continue 
-        visited.add(i)
-        dfs(i,target,len+1)
-     
+# t_depth=depth[k]
+for key,value in depth.items():
+    if value==depth[k] and parent[key]!=parent[k]:
+        ans+=1
+
+print(ans)
