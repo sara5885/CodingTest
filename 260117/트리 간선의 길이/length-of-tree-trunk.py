899f6951bc50a1c8da33db1f1eced2483c62ddc0
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-17 14:22:41 +0900

    Title: 트리 간선의 길이, Time: 141ms, Memory: 23MB, Status: Passed - Codetree

--- 260117/트리 간선의 길이/length-of-tree-trunk.py
+++ 260117/트리 간선의 길이/length-of-tree-trunk.py
@@ -12,27 +12,49 @@ for i,j, w in edges:
 
 ans=0
 
-def compute_dist(i,j):
-    visited=[0]*(10001)
-    dist=[0]*(10001)
-    visited[i]=1
-    pq=deque([i])
-    while pq:
-        node=pq.popleft()
-        if node==j: return dist[node]
-        for child,weight in graph[node]:
-            if not visited[child]:
-                visited[child]=1
-                pq.append(child)
-                dist[child]=dist[node]+weight
 
-    return -1 
+# def compute_dist(i,j):
+#     visited=[0]*(10001)
+#     dist=[0]*(10001)
+#     visited[i]=1
+#     pq=deque([i])
+#     while pq:
+#         node=pq.popleft()
+#         if node==j: return dist[node]
+#         for child,weight in graph[node]:
+#             if not visited[child]:
+#                 visited[child]=1
+#                 pq.append(child)
+#                 dist[child]=dist[node]+weight
+
+#     return -1 
         
 
-for i in range(1,n):
-    for j in range(i+1,n+1):
-        dist=compute_dist(i,j)
-        if dist!=-1:
-            ans=max(ans,dist)
+# for i in range(1,n):
+#     for j in range(i+1,n+1):
+#         dist=compute_dist(i,j)
+#         if dist!=-1:
+#             ans=max(ans,dist)
 
+# print(ans)
+
+# bfs 두번 돌리기 
+def bfs(i):
+    pq=deque([(i,0)])
+    visited=[0]*(10001)
+    max_node=0
+    max_dist=0
+    while pq:
+        c_node,c_weight=pq.popleft()
+        for n_node, n_weight in graph[c_node]:
+            if not visited[n_node]:
+                visited[n_node]=1
+                pq.append((n_node,n_weight+c_weight))
+                if n_weight+c_weight > max_dist:
+                    max_node=n_node
+                    max_dist=n_weight+c_weight 
+    return max_node, max_dist 
+
+n,d=bfs(1)
+_,ans=bfs(n)
 print(ans)
