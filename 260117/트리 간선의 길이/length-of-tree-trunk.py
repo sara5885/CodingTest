c401a82a7b678433f02270fa3132696a32cca9b3
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-17 14:12:04 +0900

    Title: 트리 간선의 길이, Time: 2070ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

---
+++ 260117/트리 간선의 길이/length-of-tree-trunk.py
@@ -0,0 +1,37 @@
+# 260117 (14:02)
+import sys 
+INT_MAX=sys.maxsize 
+from collections import deque
+n = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+
+graph=[[] for _ in range(n+1)]
+for i,j, w in edges:
+    graph[i].append((j,w))
+    graph[j].append((i,w))
+
+ans=0
+
+def compute_dist(i,j):
+    visited=[0]*(n+1)
+    dist=[0]*(n+1)
+    visited[i]=1
+    pq=deque([i])
+    while pq:
+        node=pq.popleft()
+        if node==j: return dist[node]
+        for child,weight in graph[node]:
+            if not visited[child]:
+                visited[child]=1
+                pq.append(child)
+                dist[child]=dist[node]+weight
+
+    return INT_MAX
+        
+
+for i in range(1,n):
+    for j in range(i+1,n+1):
+        dist=compute_dist(i,j)
+        ans=max(ans,dist)
+
+print(ans)
