46f6c0185ebed90e8dfe2d0834a79a84154f27d5
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-01-27 17:20:37 +0900

    Title: 트리 중위 순회, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 260127/트리 중위 순회/tree-inorder.py
+++ 260127/트리 중위 순회/tree-inorder.py
@@ -1,28 +1,22 @@
-# 260127 (16:17)
-import math
 K = int(input())
-inorder_traversal = list(map(int, input().split()))
+n=(1<<K)-1 #(1<<K)는 2*i를 의미 
+arr = [0]+list(map(int, input().split()))
+tree=[0]*(n+1)
+cnt=1
+def inorder(num):
+    global cnt 
 
-center=len(inorder_traversal)//2
-start=0
-end=len(inorder_traversal)
-depth=0
-depth_max=2**depth
-depth_cnt=0
-tree=[[] for _ in range(K)] # depth 개수만큼 2차원 list 
-# print(tree)
+    # num이 n보다 크면 빠져나가기
+    if num>n: return 
+    inorder(2*num)
+    tree[num]=arr[cnt]
+    cnt+=1
+    inorder(2*num+1)
 
-def inorder(arr,depth):
-    # depth update 조건에 부합하면 depth+=1 , 해당 depth의 노드 개수 : 2**depth 
-    if not arr :
-        return 
-    center=len(arr)//2
-    tree[depth].append(arr[center])
-    inorder(arr[:center],depth+1)
-    inorder(arr[center+1:], depth+1)
-inorder(inorder_traversal, 0)
+inorder(1)
 
-for row in tree:
-    for col in row:
-        print(col, end=" ")
+for i in range(1,K+1): #depth만큼
+    for j in range(1<<(i-1), 1<<i) : # 2**(i-1)부터 2**(i)까지 
+        print(tree[j],end=" ")
     print()
+
