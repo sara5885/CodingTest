8bc08df185c74ea5711415a4c2ba2d0f71894abe
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-02-18 16:30:25 +0900

    Title: 최단 Run Length 인코딩, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260218/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,28 @@
+# 260218 (16:06)
+import sys
+INT_MAX=sys.maxsize 
+A = input()
+
+# Please write your code here.
+ans=INT_MAX 
+tmp_arr=A
+for i in range(1,len(A)): #[1:n-1]~ [n-1:1] 
+    # i만큼 shift : 뒤에서부터 i개가 앞으로 온 것 
+    tmp_arr2=tmp_arr[-1]+tmp_arr[:len(A)-1]
+    tmp_arr=tmp_arr2
+    tmp_cnt=1
+    new_arr=tmp_arr[0]
+    # print(new_arr)
+    for j in range(1,len(A)):
+        if tmp_arr[j]!=tmp_arr[j-1]: #다름 update 진행
+            new_arr=new_arr+str(tmp_cnt)
+            new_arr=new_arr+tmp_arr[j]
+            tmp_cnt=1
+        else: #같음
+            tmp_cnt+=1
+        # print(new_arr)
+    new_arr=new_arr+str(tmp_cnt)
+    ans=min(ans,len(new_arr))
+
+
+print(ans)
