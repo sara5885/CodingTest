e4db52db6b4e6bb8c935d7729b3579d7d4f839cf
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-02-26 18:37:31 +0900

    Title: 1차원 폭발 게임, Time: 1095ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 260226/1차원 폭발 게임/The-1D-bomb-game.py
+++ 260226/1차원 폭발 게임/The-1D-bomb-game.py
@@ -17,12 +17,20 @@ else:
         for n in range(1,len(numbers)):
             if numbers[n]==numbers[n-1]:
                 cnt+=1
-                if cnt>=m: # 터질 폭탄들 
+                bomb_flag=True
+                # if cnt>=m: # 터질 폭탄들 
+                #     # start_i부터 현재까지의 폭탄들 제거 
+                #     # 바로 제거하면 안됨 => 끝까지 봐야함  
+                #     new_numbers=numbers[:start_i]+numbers[n+1:]
+                #     cnt=1
+                #     bomb_flag=True
+            else: #다를 때 이전 보기 
+                if bomb_flag and cnt>=m: # 터질 폭탄들 
                     # start_i부터 현재까지의 폭탄들 제거 
-                    new_numbers=numbers[:start_i]+numbers[n+1:]
+                    # 바로 제거하면 안됨 => 끝까지 봐야함  
+                    new_numbers=numbers[:start_i]+numbers[n:]
                     cnt=1
-                    bomb_flag=True
-            else:
+                
                 cnt=1 # 이전 폭탄이랑 숫자 다름 => cnt 1로 초기화 
                 start_i=n #시작 point 현재 num으로 update 
         if not bomb_flag : break
