6462e464be6cbc03881f04ae1e21228840c1950d
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-02-27 16:44:28 +0900

    Title: 최단 거리 3, Time: 235ms, Memory: 28MB, Status: Passed - Codetree

--- 260227/최단 거리 3/shortest-distance-3.py
+++ 260227/최단 거리 3/shortest-distance-3.py
@@ -5,21 +5,23 @@ n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 A, B = map(int, input().split())
 
-dist=[INT_MAX]*(n+1)
-pq=[]
-heapq.heappush(pq,(A,0))
-graph=[[] for _ in range(n+1)]
+if A==B : print(0)
+else:
+    dist=[INT_MAX]*(n+1)
+    pq=[]
+    heapq.heappush(pq,(A,0))
+    graph=[[] for _ in range(n+1)]
 
-for i,j,w in edges:
-    graph[i].append([j,w])
-    graph[j].append([i,w])
+    for i,j,w in edges:
+        graph[i].append([j,w])
+        graph[j].append([i,w])
 
-while pq:
-    c_node, c_dist = heapq.heappop(pq)
-    for n_node, n_weight in graph[c_node]:
-        cand=c_dist+n_weight 
-        if cand < dist[n_node]:
-            dist[n_node]=cand
-            heapq.heappush(pq,(n_node,cand))
+    while pq:
+        c_node, c_dist = heapq.heappop(pq)
+        for n_node, n_weight in graph[c_node]:
+            cand=c_dist+n_weight 
+            if cand < dist[n_node]:
+                dist[n_node]=cand
+                heapq.heappush(pq,(n_node,cand))
 
-print(dist[B])
+    print(dist[B])
