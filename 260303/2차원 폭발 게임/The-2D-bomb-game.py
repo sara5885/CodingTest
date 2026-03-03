7dd52de8c5f96d8ace79f8c1f568eb454b4d5420
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-03 18:49:21 +0900

    Title: 2차원 폭발 게임, Time: 570ms, Memory: 25MB, Status: Wrong Answer - Codetree

--- 260303/2차원 폭발 게임/The-2D-bomb-game.py
+++ 260303/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,4 +1,5 @@
 # 260303 (16:58)
+import copy 
 n, m, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
@@ -8,31 +9,52 @@ def drop(arr):
     for j in range(n):
         piv=n-1
         for i in range(n-1,-1,-1):
-            if new_arr[piv][j]!=0:
+            if arr[i][j]!=0:
+                new_arr[piv][j]=arr[i][j]
                 piv-=1 
-            new_arr[piv][j]=arr[i][j]
+                
     return new_arr
 
 
 def bomb(arr,m):
     # arr에서 m개 이상 같은 숫자 (column 기준) 있으면 터뜨려서 보내기 
-    new_arr=arr.copy()
+    new_arr=copy.deepcopy(arr)
     
     # pointer 2개 쓰기 
-    for j in range(n):
+    # for j in range(n):
+    #     start=0 
+    #     end=0
+    #     for i in range(n):
+            
+    #         if arr[i][j]==arr[start][j]: 
+    #             end=i
+    #         else:
+    #             # 0이면 비어있는거라 X 
+    #             if arr[start][j]!=0 and end-start+1>=m:
+    #                 for idx in range(start, end+1):
+    #                     new_arr[idx][j]=0
+    #             start=i
+    #             end=i
+    #     if arr[start][j]!=0 and end-start+1>=m:
+    #         for idx in range(start,n):
+    #             new_arr[idx][j]=0
+
+    for j in range(n): 
         start=0 
-        end=0
-        for i in range(n):
+        end=0 
+        for i in range(n): 
             if arr[i][j]==arr[start][j]: 
-                end=i
-            else:
-                if end-start+1>=m:
-                    for idx in range(start, end+1):
-                        new_arr[idx][j]=0
-                start=i
-            if i==n-1 and end-start+1>=m:
-                for idx in range(start,n):
-                    new_arr[idx][j]=0
+                end=i 
+            else: 
+                if end-start+1>=m: 
+                    for idx in range(start, end+1): 
+                        new_arr[idx][j]=0 
+                start=i 
+        if i==n-1 and end-start+1>=m: 
+            for idx in range(start,n):
+                new_arr[idx][j]=0
+
+
     # for i in new_arr:
     #     for j in i:
     #         print(j,end=" ")
@@ -53,9 +75,12 @@ def rotate(arr):
     new_arr=drop(new_arr)
     return new_arr
 
-for cnt in range(k) :
+for cnt in range(k+1) :
     # print("-------------",cnt,"-----------")
     new_arr=bomb(arr,m)
+    while new_arr!=arr:
+        arr=new_arr
+        new_arr=bomb(arr,m)
     # for i in new_arr:
     #     for j in i:
     #         print(j,end=" ")
@@ -67,10 +92,6 @@ for cnt in range(k) :
     #         print(j,end=" ")
     #     print()
     
-    if cnt==k-1:
-        tmp_arr=bomb(arr,m)
-        if tmp_arr!=arr:
-            arr=tmp_arr 
 ans=0
 for i in arr:
     for j in i:
