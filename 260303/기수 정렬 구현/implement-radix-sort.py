5347a78b6461bf311f869fee4d23c97d5cce3c2f
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-03 14:22:29 +0900

    Title: 기수 정렬 구현, Time: 131ms, Memory: 26MB, Status: Passed - Codetree

--- 260303/기수 정렬 구현/implement-radix-sort.py
+++ 260303/기수 정렬 구현/implement-radix-sort.py
@@ -7,11 +7,14 @@ max_len=len(str(max(arr)))
 # print(max_len)
 
 for pos in range(1,max_len+1):
-    pos=-pos
     # 자릿수에 맞춰서 넣어주기 
     new_arr=[[] for _ in range(10)]
     for num in arr:
-        new_arr[int(str(num)[pos])].append(num) #현재 자릿수의 값 
+        # 현재 pos랑 비교해서 자릿수보다 작으면 0에 append 
+        if len(str(num))<pos:
+            new_arr[0].append(num)
+        else:
+            new_arr[int(str(num)[-pos])].append(num) #현재 자릿수의 값 
     new_new_arr=[]
     for arr in new_arr:
         for num in arr:
