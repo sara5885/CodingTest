d58ff914a0d80fcf35adaad9c83967d375f14640
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-04 19:37:12 +0900

    Title: 최대 점프 횟수, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260304/최대 점프 횟수/maximum-number-of-jumps.py
@@ -0,0 +1,14 @@
+import sys 
+INT_MIN=-sys.maxsize 
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp=[INT_MIN]*n
+dp[0]=0
+for i in range(1,n):
+    for j in range(0,i): #i보다 작은 애들
+        # if dp[j]==INT_MIN: continue
+        if j+arr[j]>=i:
+            dp[i]=max(dp[i], dp[j]+1)
+
+print(max(dp))
