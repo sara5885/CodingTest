37d367314b1dfb6bf612c06e6125a8837698a7d1
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-04 19:21:44 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 196ms, Memory: 27MB, Status: Runtime Error - Codetree

--- 260304/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 260304/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,33 +1,24 @@
-# 260304 (17:58)
-import sys
-sys.setrecursionlimit(10**7)
+# 260304 (19:17)
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 dp=[[0 for _ in range(n)] for _ in range(n)]
 
-# i,j 기준으로 상하좌우 뻗어나가면서 dp 값 업데이트 하기 
-def dfs(i,j):
-    if dp[i][j]!=0:
-        return dp[i][j]
-    
-    dp[i][j]=1
-    for dx,dy in ((-1,0),(1,0),(0,1),(0,-1)):
-        nx,ny=i+dx, j+dy 
-        if 0<=nx<n and 0<=ny<n:
-            if grid[nx][ny]> grid[i][j]:
-                dp[i][j]=max(dp[i][j], 1+dfs(nx,ny) )
+def dfs(x,y):
+    if dp[x][y]!=0:
+        return dp[x][y]
+    dp[x][y]=1 
 
-    return dp[i][j]
+    for dx,dy in ((1,0),(-1,0),(0,1),(0,-1)):
+        nx,ny=x+dx, y+dy 
+        if 0<=nx<n and 0<=ny<n :
+            if grid[nx][ny]> grid[x][y]:
+                dp[x][y]=max(dp[x][y], dfs(nx,ny)+1)
+    return dp[x][y]
 
-ans=0
+ans=0 
 for i in range(n):
     for j in range(n):
         ans=max(ans, dfs(i,j))
+
 print(ans)
-
-# for row in dp:
-#     for col in row:
-#         print(col,end=" ")
-#     print()
-
