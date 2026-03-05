16fbd2fd7c558af59940d3db4de1ee043268fb17
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-05 14:13:56 +0900

    Title: 최장 공통 부분 수열, Time: 90ms, Memory: 25MB, Status: Passed - Codetree

--- 260305/최장 공통 부분 수열/longest-common-sequence.py
+++ 260305/최장 공통 부분 수열/longest-common-sequence.py
@@ -1,4 +1,4 @@
-# 260305 (13:43)
+# 260305 (13:43) (14:11)
 A = input()
 B = input()
 
@@ -30,7 +30,8 @@ for i in range(1,len(B)):
 for i in range(1,len(A)):
     for j in range(1,len(B)):
         if A[i]==B[j]: 
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]+1)
+            # dp[i][j]=max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]+1)
+            dp[i][j]=dp[i-1][j-1]+1
         else:
             dp[i][j]=max(dp[i-1][j], dp[i][j-1])
 
