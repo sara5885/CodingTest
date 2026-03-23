885cb9d10820f95daad35bbd93a175facbfbfa97
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-23 22:38:19 +0900

    Title: 벽이 없는 충돌 실험, Time: 660ms, Memory: 364MB, Status: Memory Limit Exceed - Codetree

--- 260323/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 260323/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,58 +1,64 @@
+# 260323 (22:04)
+import sys 
+INT_MAX=sys.maxsize
 T = int(input())
-
-dir={'R':(1,0),'L':(-1,0),'U':(0,1),'D':(0,-1)}
-
+dir={'U':(0,1),'D':(0,-1), 'L':(-1,0),'R':(1,0)}
 for _ in range(T):
     N = int(input())
-    jewels = []
+    x, y, w, d = [], [], [], []
+    jewels=[[] for _ in range(N)] #[x,y,index,power,direction]
+    grid=[[-1 for _ in range(4001)] for _ in range(4001)] # 4001로 해도 됨. 어차피 그 이상이면 out of index
+
     for i in range(N):
         xi, yi, wi, di = input().split()
-        jewels.append([int(xi), int(yi), int(wi), di, i+1])
-    
-    events = []
-    for i in range(N):
-        for j in range(i+1, N):
-            x1,y1,w1,d1,i1 = jewels[i]
-            x2,y2,w2,d2,i2 = jewels[j]
-            dx1,dy1 = dir[d1]
-            dx2,dy2 = dir[d2]
-            
-            ddx = dx1-dx2
-            ddy = dy1-dy2
-            
-            if ddx == 0 and ddy == 0:
-                continue
-            
-            if ddx == 0:
-                if x1 != x2: continue
-                if ddy == 0 or (y2-y1) % ddy != 0: continue
-                t = 2*(y2-y1) // ddy
-            elif ddy == 0:
-                if y1 != y2: continue
-                if (x2-x1) % ddx != 0: continue
-                t = 2*(x2-x1) // ddx
+        x.append(2*int(xi)+2000)
+        y.append(2*int(yi)+2000)
+        w.append(int(wi))
+        d.append(di)
+        jewels[i]=[2*int(xi)+2000, 2*int(yi)+2000, i, int(wi), di]
+
+    # x[],y[],w[],d[]는 idx에 의해 
+    # 이동 -> 충돌 (초마다 업데이트)
+    final_collision=-1
+    for i in range(1,4001): #(-1000~1000)*2 
+        #1. 구슬 이동 
+        #2. 충돌 확인 
+        # (X)=> 모든 구슬 이동 => 모든 구슬 충돌 x 
+        # (O)=> 모든 구슬에 대해 : 이동&충돌확인 
+        # ** 구슬은 어떻게 관리할 것인가? => 매번 새로운 구슬 list?
+        # ** new_jewels를 만드는게 나을까 아니면 원래 jewels의 값을 update 하는게 나을까
+        new_jewels=[]*N
+        for cx,cy,cidx,cw,cdir in jewels:
+            if (cx,cy)==(INT_MAX,INT_MAX): #이미 사라진 구슬 
+                continue 
+            dx,dy = dir[cdir]
+            nx,ny = cx+dx, cy+dy 
+            # 1. 이동한 다음위치가 범위밖이면 skip
+            if not (0<=nx<4001 and 0<=ny<4001): 
+                continue 
+            # 2. 이동한 다음위치에 다음 구슬이 있으면 
+            if grid[nx][ny]!=-1:
+                final_collision=i
+                # print("collision!!!")
+                
+                ox,oy,oidx,ow,odir=jewels[grid[nx][ny]] # 우선순위 : cw, cidx vs 
+                # print(nx,ny, "idx1:", cidx,"idx2:",oidx)
+                # 2-1. 우선순위가 더 높으면 : update : grid[nx][ny] / 원래 new_jewels에 있는 구슬 제거 
+                if cw>ow or (cw==ow and cidx>oidx):
+                    # new_jewels에 원래 있는 값 => 꼭 제거해야하나? 
+                    grid[nx][ny]=cidx
+                    jewels[cidx]=[nx,ny,cidx,cw,cdir]
+                    jewels[oidx]=[INT_MAX,INT_MAX,oidx,ow,odir]
+                # 2-2. 우선순위가 더 낮으면 : skip / new_jewels에 update X 
+                else:
+                    jewels[cidx]=[INT_MAX,INT_MAX,cidx,cw,cdir]
+                # print("disapperad!!!")
+                # print(nx,ny, "winner:",grid[nx][ny])
+                
             else:
-                if (x2-x1) % ddx != 0: continue
-                t = 2*(x2-x1) // ddx
-                if (y2-y1) % ddy != 0: continue
-                if t != 2*(y2-y1) // ddy: continue
-            
-            if t > 0:
-                events.append((t, i, j))
+                grid[nx][ny]=cidx
+                jewels[cidx]=[nx,ny,cidx,cw,cdir]
+
+    print(final_collision)
     
-    events.sort()
-    dead = set()
-    last_collision = -1
-    
-    for t, i, j in events:
-        if i in dead or j in dead:
-            continue
-        last_collision = t
-        w1,i1 = jewels[i][2], jewels[i][4]
-        w2,i2 = jewels[j][2], jewels[j][4]
-        if (w1, i1) > (w2, i2):
-            dead.add(j)
-        else:
-            dead.add(i)
-    
-    print(last_collision)
+
