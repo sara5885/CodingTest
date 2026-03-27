f58ae5690da0d416188ed60182abac63d50d0704
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-03-27 19:06:58 +0900

    Title: 아름다운 수, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260327/아름다운 수/beautiful-number.py
+++ 260327/아름다운 수/beautiful-number.py
@@ -1,36 +1,42 @@
-
+# 변수 선언 및 입력:
 n = int(input())
-# answer=[]
-answer=list()
-ans=0
-
-def check(): #여기가 핵심 
-    # 4가지 수 
-    idx=0
-    while idx<n: #현재위치 
-        # 현재위치의 숫자 파악 
-        if idx+answer[idx]-1>=n: #out of range 
-            return False  
-
-        for j in range(idx,idx+answer[idx]):
-            if answer[idx]!=answer[j]:
-                return False 
-
-        idx+=answer[idx] #해당 거리만큼 idx에서 점프 
-    # print(*answer)
-    return True 
+ans = 0
+seq = list()
 
 
-def choose(cnt):
+def is_beautiful():
+    # 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
+    i = 0
+    while i < n:
+        # 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        # 아름다운 수가 아닙니다.
+        if i + seq[i] - 1 >= n:
+            return False
+        # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
+        # 하나라도 다른 숫자가 있다면
+        # 아름다운 수가 아닙니다.
+        for j in range(i, i + seq[i]):
+            if seq[j] != seq[i]:
+                return False
+            
+        i += seq[i]
+        
+    return True
+
+
+def count_beautiful_seq(cnt):
     global ans
-    if cnt==n:
-        if check():
-            ans+=1 
-        return 
-    for i in range(1,5):
-        answer.append(i)
-        choose(cnt+1)
-        answer.pop()
+    
+    if cnt == n:
+        if is_beautiful():
+            ans += 1
+        return
+	
+    for i in range(1, 5):
+        seq.append(i)
+        count_beautiful_seq(cnt + 1)
+        seq.pop()
 
-choose(0)
+
+count_beautiful_seq(0)
 print(ans)
