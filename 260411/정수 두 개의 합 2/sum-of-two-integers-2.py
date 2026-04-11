d079294ef17b95c4721a0b4ff546e4c08212c38a
Soojin Hwang <56674519+sara5885@users.noreply.github.com>
2026-04-11 09:50:34 +0900

    Title: 정수 두 개의 합 2, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260411/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+ans=0
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i]+arr[j]<=k:
+            ans+=1 
+print(ans)
